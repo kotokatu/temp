c78931ab3ba260f2a68da2ce9d4477d8221e9027
EternalRival <eternalrival8@gmail.com>
2023-10-21 12:16:24 +0300

    refactor: move app styles

--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -5,9 +5,6 @@ type HeaderProps = { children: ReactNode };
 
 export class Header extends Component<HeaderProps> {
   render() {
-
-
-    
-                       return <header className={styles.header}>{this.props.children}</header>;
+    return <header className={styles.header}>{this.props.children}</header>;
   }
 }
