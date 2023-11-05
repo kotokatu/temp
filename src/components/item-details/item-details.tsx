43db38efcee1c3c8f8e19d42f12edf83779d36ac
a-reznikov <a.reznnikov@gmail.com>
2023-11-05 12:43:59 +0300

    feat: add more information about characters

--- src/components/item-details/item-details.tsx
+++ src/components/item-details/item-details.tsx
@@ -9,7 +9,19 @@ const ItemDetails: React.FC<ItemDetailsProps> = ({
   itemData,
 }) => {
   function renderItem(): JSX.Element {
-    const { name, gender, race, birth, _id } = itemData[0];
+    const {
+      _id,
+      birth,
+      death,
+      gender,
+      hair,
+      height,
+      name,
+      race,
+      realm,
+      spouse,
+      wikiUrl,
+    } = itemData[0];
     return (
       <section className="section-right">
         <div className="details-card card d-flex flex-row mb-3" key={_id}>
@@ -25,10 +37,33 @@ const ItemDetails: React.FC<ItemDetailsProps> = ({
                 <span>{`Gender: ${gender}`}</span>
               </li>
               <li className="list-group-item">
+                <span>{`Race: ${race}`}</span>
+              </li>
+              <li className="list-group-item">
                 <span>{`Birth: ${birth}`}</span>
               </li>
               <li className="list-group-item">
-                <span>{`Race: ${race}`}</span>
+                <span>{`Death: ${death}`}</span>
+              </li>
+              <li className="list-group-item">
+                <span>{`Hair: ${hair}`}</span>
+              </li>
+              <li className="list-group-item">
+                <span>{`Height: ${height}`}</span>
+              </li>
+              <li className="list-group-item">
+                <span>{`Realm: ${realm}`}</span>
+              </li>
+              <li className="list-group-item">
+                <span>{`Spouse: ${spouse}`}</span>
+              </li>
+              <li className="list-group-item">
+                <span>
+                  {`Link: ${wikiUrl === 'no info' ? wikiUrl : ''}`}
+                  <a href={`${wikiUrl}`} target="_blank" rel="noreferrer">
+                    {wikiUrl !== 'no info' ? `More info` : ''}
+                  </a>
+                </span>
               </li>
             </ul>
           </div>
